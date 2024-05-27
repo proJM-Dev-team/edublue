ca04ba29c7a09c4dc033aa8470738c606c98e61a
proJM <81658610+proJM-Coding@users.noreply.github.com>
2024-05-27 08:18:07 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 
-rpm-ostree uninstall kde-connect
+# rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
