1458b2b4670deb73446a10519b63f0163e7cdeca
proJM <81658610+proJM-Coding@users.noreply.github.com>
2024-05-27 08:28:26 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,9 +14,10 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 
-# rpm-ostree install screen
+rpm-ostree install firefox
 
 # this would install a package from rpmfusion
+
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
