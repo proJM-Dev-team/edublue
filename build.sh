b620a2ece77ea4ea4a398da7b6096b46a72ff2ae
proJM <81658610+proJM-Coding@users.noreply.github.com>
2024-06-09 04:43:49 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,7 @@ rpm-ostree install kwin-x11 plasma-workspace-x11
 rpm-ostree install firefox
 
 # Install docker for winapps
-rpm-ostree install https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-ce-26.1.4-1.fc40.x86_64.rpm
-rpm-ostree install https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-ce-cli-26.1.4-1.fc40.x86_64.rpm
-rpm-ostree install https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/containerd.io-1.6.33-3.1.fc40.x86_64.rpm
-rpm-ostree install https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-buildx-plugin-0.14.1-1.fc40.x86_64.rpm
-rpm-ostree install https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-compose-plugin-2.27.1-1.fc40.x86_64.rpm
+rpm-ostree install https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-ce-26.1.4-1.fc40.x86_64.rpm https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-ce-cli-26.1.4-1.fc40.x86_64.rpm https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/containerd.io-1.6.33-3.1.fc40.x86_64.rpm https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-buildx-plugin-0.14.1-1.fc40.x86_64.rpm https://download.docker.com/linux/fedora/40/x86_64/stable/Packages/docker-compose-plugin-2.27.1-1.fc40.x86_64.rpm
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
